863a1e3eb7b2250db3520a297bae31decc0055ce
yukti Goswami <yuktigo05@gemail.come>
2022-11-19 15:10:21 +0530

    Assignment: Loop

--- block-BJaaay/code/index.js
+++ block-BJaaay/code/index.js
@@ -4,10 +4,39 @@
 
 // Using `console.log` log all the values from 1 to 10.
 
+for(let i = 1; i < 10; i = i + 1){
+        console.log (i);
+}
+
 // Using `console.log` log all the evan values from 1 to 10.
+
+for(let i = 1; i < 10; i = i + 1){
+    if (i % 2 == 0){
+        console.log (i);
+    }
+}
 
 // Using `console.log` log all the od values from 1 to 10.
 
+for(let i = 1; i < 10; i = i + 1){
+    if (i % 2 !== 0){
+        console.log(i);
+    }
+}
 // Calculate the sum of all numbers from 1 to 10.
 
+let i = 0;
+let sum = 0;
+for (i = 1; i <= 10; i++){
+    sum = sum + i;
+}
+document.write(sum);
+
 // Log all the values from 1 to 10 using while loop
+
+let a = 1;
+while(a < 10){
+        console.log(a);
+    a = a + 1;
+}
+
